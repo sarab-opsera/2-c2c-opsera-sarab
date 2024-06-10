91da3adb2a55309330acecd76fa6a7dd6a95239b
sarab-opsera <153240837+sarab-opsera@users.noreply.github.com>
2024-06-10 14:52:55 -0700

    Dockerfile name is build.Dockerfile
    add all the js files.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Amazon Micro Service</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Demo June 6th , test 2
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
